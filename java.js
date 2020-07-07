69eaea2f9e618f4c03c3d072421a88ceab45fd8d
unknown <yoanna.manolcheva@yahoo.com>
2020-07-07 11:43:48 +0100

    Adding comments

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
 const resultArr = [];
 const initialArr = [1, 2, 4, 591, 392, 391, 2, 5, 10, 2, 1, 1, 1, 20, 20];
 initialArr.sort(function(a, b) { return a - b });
-//console.log(initialArr);
+console.log(initialArr);
 
 const modifyArr = (arr) => {
     let tempArr = [];
